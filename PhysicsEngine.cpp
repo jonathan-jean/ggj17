f50d8a6c2a86a0576d6839550f60ef816b68f335
arnoul_r <remi.arnould@epitech.eu>
2017-01-21 10:39:24 +0100

    PhysicsEngine Creation

---
+++ PhysicsEngine.cpp
@@ -0,0 +1,42 @@
+//
+// Created by arnoul_r on 21/01/17.
+//
+
+#include "PhysicsEngine.hh"
+
+PhysicsEngine::PhysicsEngine(float xGravity, float yGravity) {
+	this->world = new b2World(b2Vec2(xGravity, yGravity));
+	this->refreshFrequency = 1 / 60;
+}
+
+PhysicsEngine::~PhysicsEngine() {
+	delete(this->world);
+}
+
+b2Body*                 PhysicsEngine::createRectangle(int x, int y, int w, int h, bool dyn)
+{
+	b2BodyDef           def;
+	b2Body              *body;
+	b2PolygonShape      shape;
+	b2FixtureDef        fix;
+
+	def.position.Set(x, y);
+	if (dyn)
+		def.type = b2_dynamicBody;
+	body = this->world->CreateBody(&def);
+	shape.SetAsBox(w / 2, h / 2);
+	fix.shape = &shape;
+	fix.density = 1.0;
+	body->CreateFixture(&fix);
+	return body;
+}
+
+std::vector                 PhysicsEngine::getVertices(b2Body* body)
+{
+	std::vector             vector;
+
+	for (int i = 0; i <4 ; i++)
+		vector.push_back(((b2PolygonShape*)body->GetFixtureList()->GetShape())->GetVertex(i));
+	return vector;
+}
+
---
+++ PhysicsEngine.hh
@@ -0,0 +1,28 @@
+//
+// Created by arnoul_r on 21/01/17.
+//
+
+#ifndef GGJ17_PHYSICSENGINE_HH
+#define GGJ17_PHYSICSENGINE_HH
+
+#include <Box2D/Box2D.h>
+#include <vector>
+
+class PhysicsEngine {
+
+private:
+	b2World             *world;
+	float               refreshFrequency;
+
+public:
+	PhysicsEngine(float, float);
+	~PhysicsEngine();
+
+public:
+	b2Body*             createRectangle(int, int, int, int, bool = true);
+	static std::vector  getVertices(b2Body *);
+
+};
+
+
+#endif //GGJ17_PHYSICSENGINE_HH
