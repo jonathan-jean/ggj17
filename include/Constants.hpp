8d3f7d11df903784717e94f52b5ec927d230ca09
tonell_m <marceau.tonelli@epitech.eu>
2017-01-21 02:02:00 +0100

    base SDL test

---
+++ include/Constants.hpp
@@ -0,0 +1,12 @@
+//
+// Created by tonell_m on 21/01/17.
+//
+
+#ifndef BASIC_CONSTANTS_HPP
+#define BASIC_CONSTANTS_HPP
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+#endif //BASIC_CONSTANTS_HPP
---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+#include "SDL2/SDL.h"
+#include "SDLUtils.hh"
+#include "Constants.hh"
+#include <iostream>
+
+int main() {
+
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+	SDL_Texture *tex;
+	bool quit;
+	SDL_Event event;
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+		logSDLError(std::cout, "SDL_Init");
+		return 1;
+	}
+
+	window = SDL_CreateWindow("Lesson 2", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (window == nullptr){
+		logSDLError(std::cout, "CreateWindow");
+		SDL_Quit();
+		return 1;
+	}
+
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == nullptr){
+		logSDLError(std::cout, "CreateRenderer");
+		cleanup(window);
+		SDL_Quit();
+		return 1;
+	}
+
+	if ((tex = loadTexture("resources/bmp/square300.bmp", renderer)) == nullptr)
+	{
+		return 1;
+	}
+
+	// MainLoop
+	while (!quit)
+	{
+		SDL_WaitEvent(&event);
+
+		switch (event.key.keysym.sym)
+		{
+			case SDLK_ESCAPE:
+				quit = true;
+				break;
+		}
+
+		//First clear the renderer
+		SDL_RenderClear(renderer);
+		//Draw the texture
+		renderTexture(tex, renderer);
+		//Update the screen
+		SDL_RenderPresent(renderer);
+	}
+
+	//Cleanup
+	cleanup(window, renderer, tex);
+}
